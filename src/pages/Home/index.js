5b0ef2d7ff7aee2595ee70606540dddfb43a4f9c
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-08-05 15:55:15 -0300

    initial commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
+import categoriasRepository from '../../repositories/categorias';
+
 function Home() {
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div style={{ background: "#141414"}}>
+    <div style={{ background: '#141414' }}>
       <Menu />
 
-      <BannerMain 
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={`Go está se tornando cada vez mais popular 
         como linguagem preferida para criar aplicativos da Web.`}
       />
 
-      <Carousel 
+      <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[1]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[2]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[3]}
       />
       <Footer />
