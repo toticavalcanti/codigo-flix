396c143ab689ea04b21f5a6fabb96409f8857d2b
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-08-14 13:15:49 -0300

    video management with React and JavaScript

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,77 @@
-import React from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+/* eslint-disable linebreak-style */
+import React, { useEffect, useState } from 'react';
+//import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
-function Home() {
-  return (
-    <div style={{ background: "#141414"}}>
-      <Menu />
+import PageDefault from '../../components/PageDefault';
+import categoriasRepository from '../../repositories/categorias';
 
-      <BannerMain 
+function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        // eslint-disable-next-line no-console
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
+
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
+
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={`Go está se tornando cada vez mais popular 
         como linguagem preferida para criar aplicativos da Web.`}
       />
 
-      <Carousel 
+      <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[1]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[2]}
       />
-      <Carousel 
+      <Carousel
         category={dadosIniciais.categorias[3]}
-      />
-      <Footer />
-    </div>
+      /> */}
+
+    </PageDefault>
   );
 }
 
