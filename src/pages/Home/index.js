92296e625f958413074d675c4669b9134a7894d0
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-08-14 13:19:51 -0300

    video management with React and JavaScript

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
-import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
-import categoriasRepository from '../../repositories/categorias';
-
-function Home() {
-  useEffect(() => {
-    categoriasRepository.getAllWithVideos()
-      .then((categoriasComVideos) => {
-        console.log(categoriasComVideos);
-      })
-      .catch((err) => {
-=======
 /* eslint-disable linebreak-style */
 import React, { useEffect, useState } from 'react';
 //import dadosIniciais from '../../data/dados_iniciais.json';
@@ -34,18 +17,11 @@ function Home() {
       })
       .catch((err) => {
         // eslint-disable-next-line no-console
->>>>>>> helloworld
         console.log(err.message);
       });
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div style={{ background: '#141414' }}>
-      <Menu />
-
-      <BannerMain
-=======
     <PageDefault paddingAll={0}>
       {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
@@ -75,7 +51,6 @@ function Home() {
       })}
 
       {/* <BannerMain
->>>>>>> helloworld
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={`Go está se tornando cada vez mais popular 
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,8 +2,6 @@
 import config from '../config';
 
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`;
-<<<<<<< HEAD
-=======
 
 function getAll() {
   return fetch(`${URL_CATEGORIES}`)
@@ -17,7 +15,6 @@ function getAll() {
     });
 }
 
->>>>>>> helloworld
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaDoServidor) => {
@@ -25,18 +22,11 @@ function getAllWithVideos() {
         const resposta = await respostaDoServidor.json();
         return resposta;
       }
-<<<<<<< HEAD
-=======
-
->>>>>>> helloworld
       throw new Error('Não foi possível pegar os dados :(');
     });
 }
 
 export default {
   getAllWithVideos,
-<<<<<<< HEAD
-=======
   getAll,
->>>>>>> helloworld
 };
